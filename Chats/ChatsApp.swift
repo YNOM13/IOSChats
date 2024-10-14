639f4d44c602fd19a10beb392501c2693e986c4e
Yaroslav <Jarik239980@gmail.com>
2024-10-14 21:50:01 +0300

    Initial Commit

---
+++ Chats/ChatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatsApp.swift
+//  Chats
+//
+//  Created by Yaroslav on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
